dc758d8e61e5f7b3fbd2f8336346d831e147f721
Marc Brinkmann <git@marcbrinkmann.de>
2018-11-07 17:52:28 +0100

    Completed documentation, bumped version.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! A 'snitching' atomically reference counted pointer.
 //!
-//! [`Narc`]s are almost transparent wrappers around atomic reference counts ([`std::sync::Arc`])
+//! `Narc`s are almost transparent wrappers around atomic reference counts (`std::sync::Arc`)
 //! that track where references originated from. By recording the initial creation, as well as any
 //! operation that results in a new reference, such as a cloning or downgrading, any reference is
 //! always able to tell what its siblings pointing to the same value are.
@@ -8,7 +8,12 @@
 //! This functionality is useful for manually tracking down reference cycles or other causes that
 //! prevent proper clean-up, which occasionally result in deadlocks.
 //!
-//! TODO: Example on how to use.
+//! `Narc` and `Weak` are drop-in replacements for `Arc` and `Weak` respectively. However, calling
+//! the standard interfaces does not allow complete call site information to be added, so methods
+//! like `new_at_line` or `clone_at_line` should be used. In case the compatible methods like `new`,
+//! `clone`, ... are called, `Site::Unknown` is used for the resulting tracked `Origin`.
+//!
+//! TODO: Example on how to use, including `file!` and `line!` macros.
 
 pub mod tracing;
 
@@ -23,13 +28,17 @@ use tracing::{Origin, OriginKind, Site, Uid};
 /// created via `clone` or `downgrade` is tracked by being assigned a unique ID as well. If the
 /// annotating methods `new_at_line`, `clone_at_line`, etc. are used, the `Narc` will also know
 /// its origin.
-
 #[derive(Debug)]
 pub struct Narc<T> {
+    /// Wrapped [std::sync] arc reference.
     inner: Arc<Inner<T>>,
+    /// Unique ID for this instance.
     id: Uid,
 }
 
+/// Tracked reference state.
+///
+/// The `Map` tracks the number and site of references pointing toward the same value.
 #[derive(Debug)]
 struct Map {
     strongs: HashMap<Uid, Origin>,
@@ -38,6 +47,7 @@ struct Map {
 }
 
 impl Map {
+    /// Creates a new map instance.
     fn new() -> Map {
         Map {
             strongs: HashMap::with_capacity(128),
@@ -46,7 +56,7 @@ impl Map {
         }
     }
 
-    // Increments the `next_id` counter and returns the previous value.
+    /// Increments the `next_id` counter and returns the previous value.
     fn next_id(&mut self) -> Uid {
         let id = self.next_id;
         self.next_id += 1;
@@ -54,20 +64,28 @@ impl Map {
     }
 }
 
+/// Inner state of `Narc`.
 #[derive(Debug)]
 struct Inner<T> {
+    /// The actual value.
     data: T,
+    /// Sibling metadata.
     map: Mutex<Map>,
 }
 
 /// The non-owned version of a `Narc`.
 #[derive(Debug)]
 pub struct Weak<T> {
+    /// Wrapped non-owned [std::sync] arc reference.
     inner: ArcWeak<Inner<T>>,
+    /// Unique ID for this instance.
     id: Uid,
 }
 
 impl<T> Narc<T> {
+    /// Internal instantiation function.
+    ///
+    /// Directly accepts a `Site` instance, creates the correct `Origin` with `OriginKind::New`.
     fn new_at_site(data: T, site: Site) -> Narc<T> {
         let origin = Origin {
             kind: OriginKind::New,
@@ -87,12 +105,16 @@ impl<T> Narc<T> {
         }
     }
 
+    /// Internal cloning function.
+    ///
+    /// Directly accepts a `Site` instance, creates the correct `Origin` with
+    /// `OriginKind::ClonedFrom`.
     fn clone_at_site(&self, site: Site) -> Narc<T> {
         let mut map = self.inner.map.lock().unwrap();
         let parent_origin = map
             .strongs
             .get(&self.id)
-            .expect("Internal consistency error (clone)")
+            .expect("Internal consistency error (clone). This should never happen.")
             .clone();
         let new_origin = Origin {
             kind: OriginKind::ClonedFrom(self.id, Box::new(parent_origin)),
@@ -107,13 +129,17 @@ impl<T> Narc<T> {
         }
     }
 
+    /// Internal downgrade function.
+    ///
+    /// Directly accepts a `Site` instance, creates the correct `Origin` with
+    /// `OriginKind::DowngradedFrom`.
     fn downgrade_at_site(this: &Self, site: Site) -> Weak<T> {
         let mut map = this.inner.map.lock().unwrap();
         // No need to `::remove` here because the strong ref will be dropped.
         let prev_origin = map
             .strongs
             .get(&this.id)
-            .expect("Internal consistency error (downgrade)")
+            .expect("Internal consistency error (downgrade). This should never happen.")
             .clone();
         let new_origin = Origin {
             kind: OriginKind::DowngradedFrom(this.id, Box::new(prev_origin)),
@@ -128,46 +154,62 @@ impl<T> Narc<T> {
         }
     }
 
-    /// Returns a new `Narc` with the provided file name and line as the 'origin'.
+    /// Returns a new `Narc` with the provided file name and line as the origin.
     pub fn new_at_line(data: T, file: &'static str, line: u32) -> Narc<T> {
         Narc::new_at_site(data, Site::SourceFile { file, line })
     }
 
-    /// Creates a new [`Weak`][weak] pointer to this value.
+    /// Creates a new `Weak` pointer to this value.
     pub fn clone_at_line(&self, file: &'static str, line: u32) -> Narc<T> {
         self.clone_at_site(Site::SourceFile { file, line })
     }
 
-    /// Creates a new [`Weak`][weak] pointer to this value.
+    /// Creates a new `Weak` pointer to this value.
     pub fn downgrade_at_line(this: &Self, file: &'static str, line: u32) -> Weak<T> {
         Narc::downgrade_at_site(this, Site::SourceFile { file, line })
     }
 
-    // TODO: Come up with a clever way to impl this.
-    //
-    // /// Returns the contained value if the `Narc` has exactly one strong reference.
-    // pub fn try_unwrap(this: Self) -> Result<T, Self> {
-    //     Arc::try_unwrap(this.inner)
-    //         .map(|i| i.data)
-    //         .map_err(|i| Narc { inner: i })
-    // }
+    /// Returns the contained value if the `Narc` has exactly one strong reference.
+    pub fn try_unwrap(_this: Self) -> Result<T, Self> {
+        // TODO: Come up with a clever way to impl this.
+        // Arc::try_unwrap(this.inner)
+        //     .map(|i| i.data)
+        //     .map_err(|i| Narc { inner: i })
+        unimplemented!()
+    }
 
+    /// Gets the number of `Weak` pointers to this value.
+    ///
+    /// See `std::sync::Arc::weak_count` for details.
     pub fn weak_count(_this: &Narc<T>) -> usize {
         unimplemented!()
     }
 
+    /// Gets the number of `Narc` pointers to this value.
+    ///
+    /// See `std::sync::Arc::strong_count` for details.
     pub fn strong_count(_this: &Narc<T>) -> usize {
         unimplemented!()
     }
 
+    /// Returns true if the two Arcs point to the same value (not just values that compare as equal).
+    ///
+    /// See `std::sync::Arc::ptr_eq` for details.
     pub fn ptr_eq(_this: &Narc<T>, _other: &Narc<T>) -> bool {
         unimplemented!()
     }
 
+    /// Makes a mutable reference into the given Arc.
+    ///
+    /// See `std::sync::Arc::make_mut` for details.
     pub fn make_mut(_this: &mut Narc<T>) -> &mut T {
         unimplemented!()
     }
 
+    /// Returns a mutable reference to the inner value, if there are no other Arc or Weak pointers
+    /// to the same value.
+    ///
+    /// See `std::sync::Arc::make_mut` for details.
     pub fn get_mut(_this: &mut Narc<T>) -> Option<&mut T> {
         unimplemented!()
     }
@@ -190,7 +232,13 @@ impl<T> Drop for Narc<T> {
     }
 }
 
+// TODO: Clone impl.
+
 impl<T> Weak<T> {
+    /// Internal upgrade function.
+    ///
+    /// Directly accepts a `Site` instance, creates the correct `Origin` with
+    /// `OriginKind::UpgradedFrom`.
     pub fn upgrade_at_site(&self, site: Site) -> Option<Narc<T>> {
         self.inner.upgrade().map(|inner| {
             let id = {
@@ -212,6 +260,10 @@ impl<T> Weak<T> {
         })
     }
 
+    /// Attempts to upgrade the Weak pointer to an Arc, extending the lifetime of the value if
+    /// successful.
+    ///
+    /// See `std::sync::Weak::upgrade` for details.
     pub fn upgrade_at_line(&self, file: &'static str, line: u32) -> Option<Narc<T>> {
         self.upgrade_at_site(Site::SourceFile { file, line })
     }
@@ -227,6 +279,8 @@ impl<T> Drop for Weak<T> {
         }
     }
 }
+
+// TODO: Implement Clone for Weak.
 
 #[cfg(test)]
 mod tests {
--- src/tracing.rs
+++ src/tracing.rs
@@ -1,24 +1,47 @@
-//! A 'snitching' atomically reference counted pointer.
+//! Tracing utilities.
+//!
+//! Data types to track origin and history across call sites.
 
+/// Unique ID type to identify ancestors.
 pub type Uid = usize;
 
+/// Call site.
 #[derive(Debug, Copy, Clone)]
 pub enum Site {
-    SourceFile { file: &'static str, line: u32 },
+    /// File/line location inside a source file.
+    SourceFile {
+        /// Source file.
+        file: &'static str,
+        /// Line number, starting at 1.
+        line: u32,
+    },
+    /// Unknown call site.
+    ///
+    /// Used, when no information about the original call site was available at runtime.
     Unknown,
     // TODO: Backtrace,
+    // TODO: Annotation,
 }
 
+/// Reference origin.
 #[derive(Debug, Clone)]
 pub enum OriginKind {
+    /// New object Instantiation.
     New,
+    /// Cloned from another reference, (original ID, site of original reference).
     ClonedFrom(Uid, Box<Origin>),
+    /// Upgraded from a weak reference, (weak reference ID, site of weak reference).
     UpgradedFrom(Uid, Box<Origin>),
+    /// Downgraded from a strong reference, (strong reference ID, site of strong reference).
     DowngradedFrom(Uid, Box<Origin>),
 }
 
+/// Describes origin and location of a new reference creation.
 #[derive(Debug, Clone)]
 pub struct Origin {
+    /// The kind of reference creation (new, via clone, downgrade, ...). In case there is a parent
+    /// instance, its origin information will be contained in the `OriginKind` instance.
     pub kind: OriginKind,
+    /// The site where the new instation occured.
     pub site: Site,
 }
